127c4913651e9bdb8d8a88278fc8f90f8ae88d72
dschegolev <dschegolev@mmtr.ru>
2021-10-15 17:49:29 +0300

    refactor slider js

--- js/utils/Slider.js
+++ js/utils/Slider.js
@@ -1,4 +1,9 @@
 class Slider {
+    slides = document.querySelectorAll(".slider__card");
+    controlDots = document.querySelector(".controls__dots");
+    labels = document.querySelectorAll(".slider__controls label");
+    arrows = document.querySelectorAll(".controls__arrows button");
+    track = document.querySelector(".stick-track");
     constructor(id, { perPage, sliderSize }) {
         this.id = id;
         this.sliderSize = sliderSize;
@@ -6,105 +11,88 @@ class Slider {
         this.countPages;
         this.currentPage = 1;
         this.currentSlide = 0;
-        //this.prevPage;
-        //this.pagesWithSlides = [];
     }
 
     mount() {
-        const slides = document.querySelectorAll(".slider__card");
-        const controlDots = document.querySelector(".controls__dots");
-        const arrows = document.querySelectorAll(".controls__arrows button");
-        const track = document.querySelector(".stick-track");
 
-        if (!slides) {
-            return;
-        }
-        //this.pagesWithSlides = Object.entries(slides);
-
-        this.setCountPage(slides);
-
-        this.addActiveClass(slides)
-
-        if (!controlDots) {
+        if (!this.slides) {
             return;
         }
 
-        this.setDataPage(controlDots);
+        this.setCountPage();
+        this.addActiveSlide()
 
-        this.checkDots(controlDots, track, slides);
+        if (!this.controlDots) {
+            return;
+        }
 
-        this.moveArrows(arrows, track, slides);
+        this.setDataPage();
+        this.checkDots();
+        this.moveArrows();
     }
 
-    checkDots(controlDots, track, slides) {
-        controlDots.addEventListener("click", (event) => {
-            //this.prevPage = this.currentPage;
+    checkDots() {
+        this.controlDots.addEventListener("click", (event) => {
+
             this.currentPage = event.target.dataset.page;
             this.currentSlide = event.target.dataset.page * this.perPage - this.perPage;
 
-            slides.forEach((slide) => { slide.classList.toggle("slider__card--active", false) })
-            this.addActiveClass(slides)
+            this.addActiveDot()
+            this.removeActiveSlide()
+            this.addActiveSlide()
 
-            //const slide = pages[this.currentPage].firstSlide;
-            //slide.classList.toggle("slider__card--active");
-
-            this.currentPage ? track.style.transform = `translate(-${this.sliderSize * (this.currentPage - 1)}px) ` : track.style.transform = "translate(0px)";
+            this.currentPage ? this.track.style.transform = `translate(-${this.sliderSize * (this.currentPage - 1)}px) ` : this.track.style.transform = "translate(0px)";
 
         });
     }
 
-    moveArrows(arrows, track, slides) {
-        arrows[0].addEventListener("click", () => {
+    moveArrows() {
+        this.arrows[0].addEventListener("click", () => {
 
             if (this.currentSlide > 0) {
-                this.removeActiveClass(slides)
+                this.removeActiveSlide()
                 this.currentSlide--
-                slides[this.currentSlide - 1].classList.toggle("slider__card--active");
-                track.style.transform = `translate(-${402 * (this.currentSlide - 1)}px) `
+                this.slides[this.currentSlide - 1].classList.toggle("slider__card--active");
+                this.track.style.transform = `translate(-${402 * (this.currentSlide - 1)}px) `
             }
 
         });
-        arrows[1].addEventListener("click", () => {
+        this.arrows[1].addEventListener("click", () => {
 
-            if (this.currentSlide < slides.length) {
-                this.removeActiveClass(slides)
-                this.addActiveClass(slides)
+            if (this.currentSlide < this.slides.length) {
+                this.removeActiveSlide()
+                this.addActiveSlide()
                 this.currentSlide++
-                track.style.transform = `translate(-${402 * (this.currentSlide - 1)}px) `
+                this.track.style.transform = `translate(-${402 * (this.currentSlide - 1)}px) `
             }
 
         });
     }
 
-    setDataPage(controlDots) {
-        const dots = controlDots.children;
-
+    setDataPage() {
+        const dots = this.controlDots.children;
         const dotsAsArray = Object.entries(dots);
 
         dotsAsArray.forEach(([key, dot]) => {
             dot.dataset.page = +key + 1;
         });
-
-        // const [dot] = dots;
-        // dot.classList.toggle("control-dot--active");
-        // dot.checked = "true";
-        // dot.addEventListener("click", () => {
-        //     dot.classList.toggle("control-dot--active");
-        // });
     }
 
-    removeActiveClass(slides) {
-        slides.forEach((slide) => { slide.classList.toggle("slider__card--active", false) })
-    }
-    
-    addActiveClass(slides) {
-        // const [firstSlide] = slides;
-        // firstSlide.classList.toggle("slider__card--active");
-        slides[this.currentSlide].classList.toggle("slider__card--active");
+    removeActiveSlide() {
+        this.slides.forEach((slide) => { slide.classList.toggle("slider__card--active", false) })
     }
 
-    setCountPage(slides) {
-        this.countPages = slides.length / this.perPage;
+    addActiveSlide() {
+        this.slides[this.currentSlide].classList.toggle("slider__card--active");
+    }
+
+    addActiveDot() {
+        this.labels.forEach((label) => { label.classList.toggle("control__dot--active", false) })
+        this.labels[this.currentPage - 1].classList.toggle("control__dot--active")
+    }
+
+    setCountPage() {
+        this.countPages = this.slides.length / this.perPage;
     }
 };
 
